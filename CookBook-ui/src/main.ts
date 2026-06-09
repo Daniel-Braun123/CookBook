6d28ad65d15817b4aa1c70e2bfb0d0a51ad7d9ba
braund <Daniel.Braun@msg.group>
2026-06-09 16:04:16 +0200

    Add app icons and manifest for PWA support
    - Added new PNG icons in various sizes (72x72, 96x96) for the application.
    - Created a web manifest file to define app metadata, including name, theme color, and icon references.

--- CookBook-ui/src/main.ts
+++ CookBook-ui/src/main.ts
@@ -6,6 +6,8 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './app/interceptors/auth.interceptor';
 import { errorInterceptor } from './app/interceptors/error.interceptor';
+import { provideServiceWorker } from '@angular/service-worker';
+import { isDevMode } from '@angular/core';
 
 bootstrapApplication(AppComponent, {
   providers: [
@@ -13,6 +15,10 @@ bootstrapApplication(AppComponent, {
     provideAnimations(),
     provideHttpClient(
       withInterceptors([authInterceptor, errorInterceptor])
-    )
+    ),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ]
 }).catch(err => console.error(err));
